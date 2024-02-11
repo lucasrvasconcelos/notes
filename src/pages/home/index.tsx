3bd57b9e1cbb8e4b194f5561a4248491192c9068
lucasrvasconcelos <lucasvasconcelos900@gmail.com>
2024-02-11 12:04:24 -0300

    modificação placeholder

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -61,7 +61,7 @@ export function Home() {
         <input
           value={seach}
           type="text"
-          placeholder="digite"
+          placeholder="Busque em suas notas"
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
           onChange={handleFilteredNotes}
         />
